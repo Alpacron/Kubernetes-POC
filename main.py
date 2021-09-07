d1643e9ae0379d81c6db074b02addd69e24b74ca
Aron Hemmes <37915022+Alpacron@users.noreply.github.com>
2021-09-07 13:38:34 +0200

    Add host id and server access times to client app

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from typing import Counter
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from starlette.middleware.cors import CORSMiddleware
+import socket;
+
+count = 0
 
 app = FastAPI(
     title="Kubernetes Demo",
@@ -8,6 +13,19 @@ app = FastAPI(
 )
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['POST', 'PUT'], allow_headers=["*"])
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def hey():
-    return "Hello World!"
+    global count
+
+    count += 1
+
+    html_content = """
+    <html>
+        <body>
+            <p>The current app is running on host with id: {}</p>
+            <p>This server has been accessed {} times.</p>
+        </body>
+    </html>
+    """.format(socket.getfqdn(), count)
+
+    return html_content
